1c9e3c182966c3867f539d7da129f844b72e9049
Fernando A. Villamizar-Moreno <fervillam07@gmail.com>
2022-03-12 22:01:24 -0500

    Update Boys-Love.sh

--- Final-project/Boys-Love.sh
+++ Final-project/Boys-Love.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-cd Documents/Ancestral-BoysLove-Project
-R
-q()
-sudo apt-get -y install python3-pip
-python3 -m pip install lagrange
-python3
-exit()
+cd Documents/Ancestral-BoysLove-Project #Setting work directory
+R #Run R console
+  if(!require("ape")){
+    install.packages("ape")
+  }
+  q() #Exit R console and returning to bash
+sudo apt-get -y install python3-pip 
+python3 -m pip install lagrange #Installing "lagrange" package
+python3 #Run Python console
+  exit() #Exit Python console and returning to bash
